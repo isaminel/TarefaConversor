db6adfce8912a2d4c54e6ddef7f1d83959844cce
Isabelle <Isabelle@LAPTOP-CN7VNT6D>
2017-08-17 20:24:43 -0300

    Commit

--- TarefaConversor/Form1.Designer.cs
+++ TarefaConversor/Form1.Designer.cs
@@ -28,12 +28,249 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelNr = new System.Windows.Forms.Label();
+            this.textNr = new System.Windows.Forms.TextBox();
+            this.labelBase = new System.Windows.Forms.Label();
+            this.radioDecimal = new System.Windows.Forms.RadioButton();
+            this.radioHexa = new System.Windows.Forms.RadioButton();
+            this.radioOctal = new System.Windows.Forms.RadioButton();
+            this.radioBinario = new System.Windows.Forms.RadioButton();
+            this.botaoConverter = new System.Windows.Forms.Button();
+            this.resultDecimal = new System.Windows.Forms.Label();
+            this.resultOctal = new System.Windows.Forms.Label();
+            this.resultHexa = new System.Windows.Forms.Label();
+            this.resultBinario = new System.Windows.Forms.Label();
+            this.boxDecimal = new System.Windows.Forms.TextBox();
+            this.boxOctal = new System.Windows.Forms.TextBox();
+            this.boxBinario = new System.Windows.Forms.TextBox();
+            this.boxHexa = new System.Windows.Forms.TextBox();
+            this.botaoSair = new System.Windows.Forms.Button();
+            this.botaoLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelNr
+            // 
+            this.labelNr.AutoSize = true;
+            this.labelNr.Location = new System.Drawing.Point(12, 22);
+            this.labelNr.Name = "labelNr";
+            this.labelNr.Size = new System.Drawing.Size(78, 13);
+            this.labelNr.TabIndex = 0;
+            this.labelNr.Text = "Insira um valor:";
+            // 
+            // textNr
+            // 
+            this.textNr.Location = new System.Drawing.Point(119, 22);
+            this.textNr.Name = "textNr";
+            this.textNr.Size = new System.Drawing.Size(100, 20);
+            this.textNr.TabIndex = 1;
+            this.textNr.TextChanged += new System.EventHandler(this.textNr_TextChanged);
+            // 
+            // labelBase
+            // 
+            this.labelBase.AutoSize = true;
+            this.labelBase.Location = new System.Drawing.Point(12, 62);
+            this.labelBase.Name = "labelBase";
+            this.labelBase.Size = new System.Drawing.Size(207, 13);
+            this.labelBase.TabIndex = 2;
+            this.labelBase.Text = "Selecione o tipo da base do valor inserido:";
+            // 
+            // radioDecimal
+            // 
+            this.radioDecimal.AutoSize = true;
+            this.radioDecimal.Location = new System.Drawing.Point(15, 93);
+            this.radioDecimal.Name = "radioDecimal";
+            this.radioDecimal.Size = new System.Drawing.Size(63, 17);
+            this.radioDecimal.TabIndex = 3;
+            this.radioDecimal.TabStop = true;
+            this.radioDecimal.Text = "Decimal";
+            this.radioDecimal.UseVisualStyleBackColor = true;
+            this.radioDecimal.CheckedChanged += new System.EventHandler(this.radioDecimal_CheckedChanged);
+            // 
+            // radioHexa
+            // 
+            this.radioHexa.AutoSize = true;
+            this.radioHexa.Location = new System.Drawing.Point(109, 93);
+            this.radioHexa.Name = "radioHexa";
+            this.radioHexa.Size = new System.Drawing.Size(86, 17);
+            this.radioHexa.TabIndex = 4;
+            this.radioHexa.TabStop = true;
+            this.radioHexa.Text = "Hexadecimal";
+            this.radioHexa.UseVisualStyleBackColor = true;
+            this.radioHexa.CheckedChanged += new System.EventHandler(this.radioHexa_CheckedChanged);
+            // 
+            // radioOctal
+            // 
+            this.radioOctal.AutoSize = true;
+            this.radioOctal.Location = new System.Drawing.Point(227, 93);
+            this.radioOctal.Name = "radioOctal";
+            this.radioOctal.Size = new System.Drawing.Size(50, 17);
+            this.radioOctal.TabIndex = 5;
+            this.radioOctal.TabStop = true;
+            this.radioOctal.Text = "Octal";
+            this.radioOctal.UseVisualStyleBackColor = true;
+            this.radioOctal.CheckedChanged += new System.EventHandler(this.radioOctal_CheckedChanged);
+            // 
+            // radioBinario
+            // 
+            this.radioBinario.AutoSize = true;
+            this.radioBinario.Location = new System.Drawing.Point(318, 93);
+            this.radioBinario.Name = "radioBinario";
+            this.radioBinario.Size = new System.Drawing.Size(57, 17);
+            this.radioBinario.TabIndex = 6;
+            this.radioBinario.TabStop = true;
+            this.radioBinario.Text = "Binário";
+            this.radioBinario.UseVisualStyleBackColor = true;
+            this.radioBinario.CheckedChanged += new System.EventHandler(this.radioBinario_CheckedChanged);
+            // 
+            // botaoConverter
+            // 
+            this.botaoConverter.Location = new System.Drawing.Point(183, 146);
+            this.botaoConverter.Name = "botaoConverter";
+            this.botaoConverter.Size = new System.Drawing.Size(75, 23);
+            this.botaoConverter.TabIndex = 7;
+            this.botaoConverter.Text = "Converter";
+            this.botaoConverter.UseVisualStyleBackColor = true;
+            this.botaoConverter.Click += new System.EventHandler(this.botaoConverter_Click);
+            // 
+            // resultDecimal
+            // 
+            this.resultDecimal.AutoSize = true;
+            this.resultDecimal.Location = new System.Drawing.Point(24, 209);
+            this.resultDecimal.Name = "resultDecimal";
+            this.resultDecimal.Size = new System.Drawing.Size(45, 13);
+            this.resultDecimal.TabIndex = 8;
+            this.resultDecimal.Text = "Decimal";
+            // 
+            // resultOctal
+            // 
+            this.resultOctal.AutoSize = true;
+            this.resultOctal.Location = new System.Drawing.Point(249, 209);
+            this.resultOctal.Name = "resultOctal";
+            this.resultOctal.Size = new System.Drawing.Size(32, 13);
+            this.resultOctal.TabIndex = 9;
+            this.resultOctal.Text = "Octal";
+            // 
+            // resultHexa
+            // 
+            this.resultHexa.AutoSize = true;
+            this.resultHexa.Location = new System.Drawing.Point(22, 251);
+            this.resultHexa.Name = "resultHexa";
+            this.resultHexa.Size = new System.Drawing.Size(68, 13);
+            this.resultHexa.TabIndex = 10;
+            this.resultHexa.Text = "Hexadecimal";
+            // 
+            // resultBinario
+            // 
+            this.resultBinario.AutoSize = true;
+            this.resultBinario.Location = new System.Drawing.Point(249, 251);
+            this.resultBinario.Name = "resultBinario";
+            this.resultBinario.Size = new System.Drawing.Size(39, 13);
+            this.resultBinario.TabIndex = 11;
+            this.resultBinario.Text = "Binário";
+            // 
+            // boxDecimal
+            // 
+            this.boxDecimal.Location = new System.Drawing.Point(95, 206);
+            this.boxDecimal.Name = "boxDecimal";
+            this.boxDecimal.Size = new System.Drawing.Size(100, 20);
+            this.boxDecimal.TabIndex = 12;
+            this.boxDecimal.TextChanged += new System.EventHandler(this.boxDecimal_TextChanged);
+            // 
+            // boxOctal
+            // 
+            this.boxOctal.Location = new System.Drawing.Point(294, 206);
+            this.boxOctal.Name = "boxOctal";
+            this.boxOctal.Size = new System.Drawing.Size(100, 20);
+            this.boxOctal.TabIndex = 13;
+            this.boxOctal.TextChanged += new System.EventHandler(this.boxOctal_TextChanged);
+            // 
+            // boxBinario
+            // 
+            this.boxBinario.Location = new System.Drawing.Point(294, 248);
+            this.boxBinario.Name = "boxBinario";
+            this.boxBinario.Size = new System.Drawing.Size(100, 20);
+            this.boxBinario.TabIndex = 14;
+            this.boxBinario.TextChanged += new System.EventHandler(this.boxBinario_TextChanged);
+            // 
+            // boxHexa
+            // 
+            this.boxHexa.Location = new System.Drawing.Point(95, 248);
+            this.boxHexa.Name = "boxHexa";
+            this.boxHexa.Size = new System.Drawing.Size(100, 20);
+            this.boxHexa.TabIndex = 15;
+            this.boxHexa.TextChanged += new System.EventHandler(this.boxHexa_TextChanged);
+            // 
+            // botaoSair
+            // 
+            this.botaoSair.Location = new System.Drawing.Point(331, 297);
+            this.botaoSair.Name = "botaoSair";
+            this.botaoSair.Size = new System.Drawing.Size(75, 23);
+            this.botaoSair.TabIndex = 16;
+            this.botaoSair.Text = "Sair";
+            this.botaoSair.UseVisualStyleBackColor = true;
+            this.botaoSair.Click += new System.EventHandler(this.botaoSair_Click);
+            // 
+            // botaoLimpar
+            // 
+            this.botaoLimpar.Location = new System.Drawing.Point(240, 297);
+            this.botaoLimpar.Name = "botaoLimpar";
+            this.botaoLimpar.Size = new System.Drawing.Size(75, 23);
+            this.botaoLimpar.TabIndex = 17;
+            this.botaoLimpar.Text = "Limpar";
+            this.botaoLimpar.UseVisualStyleBackColor = true;
+            this.botaoLimpar.Click += new System.EventHandler(this.botaoLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(418, 332);
+            this.Controls.Add(this.botaoLimpar);
+            this.Controls.Add(this.botaoSair);
+            this.Controls.Add(this.boxHexa);
+            this.Controls.Add(this.boxBinario);
+            this.Controls.Add(this.boxOctal);
+            this.Controls.Add(this.boxDecimal);
+            this.Controls.Add(this.resultBinario);
+            this.Controls.Add(this.resultHexa);
+            this.Controls.Add(this.resultOctal);
+            this.Controls.Add(this.resultDecimal);
+            this.Controls.Add(this.botaoConverter);
+            this.Controls.Add(this.radioBinario);
+            this.Controls.Add(this.radioOctal);
+            this.Controls.Add(this.radioHexa);
+            this.Controls.Add(this.radioDecimal);
+            this.Controls.Add(this.labelBase);
+            this.Controls.Add(this.textNr);
+            this.Controls.Add(this.labelNr);
+            this.Name = "Form1";
+            this.Text = "CONVERSOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelNr;
+        private System.Windows.Forms.Label labelBase;
+        private System.Windows.Forms.RadioButton radioDecimal;
+        private System.Windows.Forms.RadioButton radioHexa;
+        private System.Windows.Forms.RadioButton radioOctal;
+        private System.Windows.Forms.RadioButton radioBinario;
+        private System.Windows.Forms.Button botaoConverter;
+        private System.Windows.Forms.Label resultDecimal;
+        private System.Windows.Forms.Label resultOctal;
+        private System.Windows.Forms.Label resultHexa;
+        private System.Windows.Forms.Label resultBinario;
+        private System.Windows.Forms.TextBox boxDecimal;
+        private System.Windows.Forms.TextBox boxOctal;
+        private System.Windows.Forms.TextBox boxBinario;
+        private System.Windows.Forms.TextBox boxHexa;
+        private System.Windows.Forms.Button botaoSair;
+        private System.Windows.Forms.Button botaoLimpar;
+        public System.Windows.Forms.TextBox textNr;
     }
 }
 
--- TarefaConversor/Form1.cs
+++ TarefaConversor/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,358 @@ namespace TarefaConversor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void botaoLimpar_Click(object sender, EventArgs e)
+        {
+            boxDecimal.Text = "";
+            boxHexa.Text = "";
+            boxOctal.Text = "";
+            boxBinario.Text = "";
+            textNr.Text = "";   
+        }
+
+        private void botaoSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }        
+
+        private void radioBinario_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void radioDecimal_CheckedChanged(object sender, EventArgs e)
+        {            
+        }
+
+        private void radioOctal_CheckedChanged(object sender, EventArgs e)
+        {            
+        }
+
+        private void radioHexa_CheckedChanged(object sender, EventArgs e)
+        {           
+        }
+
+        private void botaoConverter_Click(object sender, EventArgs e)
+        {
+            
+            if (radioDecimal.Checked)
+            {
+                int nr = (Convert.ToInt32(textNr.Text));
+                
+                if (nr == 0)
+
+                boxDecimal.Text = textNr.Text;
+                boxBinario.Text = DecimalBinario(nr);
+                boxOctal.Text = DecimalOctal(nr);
+                boxHexa.Text = DecimalHexa(nr);
+
+            } else if (radioHexa.Checked)            {
+                
+                boxHexa.Text = textNr.Text;
+                boxDecimal.Text = HexaDecimal(textNr.Text);
+                boxOctal.Text = DecimalOctal(Convert.ToInt32(HexaDecimal(textNr.Text)));
+                boxBinario.Text = DecimalBinario(Convert.ToInt32(HexaDecimal(textNr.Text)));
+
+            } else if (radioOctal.Checked)
+            {
+                int nr = (Convert.ToInt32(textNr.Text));
+
+                boxOctal.Text = textNr.Text;
+                boxDecimal.Text = OctalDecimal(nr).ToString();
+                boxBinario.Text = DecimalBinario(OctalDecimal(nr));
+                boxHexa.Text = DecimalHexa(OctalDecimal(nr));
+
+            } else if (radioBinario.Checked)
+            {
+                int nr = (Convert.ToInt32(textNr.Text));
+
+                /*if (!textNr.Text.Contains('1') || textNr.Text.Contains('0'))
+                { } */
+                    boxBinario.Text = textNr.Text;
+                    boxDecimal.Text = BinarioDecimal(nr).ToString();
+                    boxOctal.Text = DecimalOctal(BinarioDecimal(nr));
+                    boxHexa.Text = DecimalHexa(BinarioDecimal(nr));
+                 
+            }
+        }
+
+        public string DecimalBinario(int nr)
+        {
+            decimal dividendo = nr;
+            decimal resto = decimal.MaxValue;
+            int quociente = int.MaxValue;
+            int divisor = 2;
+            string result = string.Empty;
+
+            List<int> listaValorBinario = new List<int>();
+
+            while (dividendo > 2)
+            {
+                resto = dividendo % divisor;
+                quociente = (int)dividendo / divisor;
+                dividendo = quociente;
+                listaValorBinario.Add(Convert.ToInt32(resto));
+            }
+
+            if (dividendo == 2)
+            {
+                listaValorBinario.Add(0);
+                listaValorBinario.Add(1);
+            } else if (dividendo == 0)
+            {
+                listaValorBinario.Add(0);
+            } else
+            {
+                listaValorBinario.Add(1);
+            }
+
+            listaValorBinario.Reverse();
+            foreach (int valor in listaValorBinario)
+            {
+                result = result + "" + valor;
+            }
+
+            return result;
+        }
+
+
+        public static string InverterString(string str) // usar no BinarioDecimal
+        {
+
+            int tamanho = str.Length;
+            char[] caracteres = new char[tamanho];
+            for (int i = 0; i < tamanho; i++)
+            {
+                caracteres[i] = str[tamanho - 1 - i];
+
+            }
+            return new string(caracteres);
+
+        }
+
+
+
+
+        public int BinarioDecimal(int nr)        {
+
+            int expoente = 0;
+            int numero;
+            int soma =  0;
+
+            string numeroInvertido = InverterString(textNr.Text);
+
+            for (int i = 0; i < numeroInvertido.Length; i++)
+            {
+
+                numero = Convert.ToInt32(numeroInvertido.Substring(i, 1));
+                soma += numero * (int)Math.Pow(2, expoente);
+                expoente++;
+
+            }
+
+            return soma;
+
+        }
+
+
+        public string DecimalOctal(int nr)
+        {
+
+            int dividendo = nr;
+            int resto = int.MaxValue;
+            int quociente = int.MaxValue;
+            int divisor = 8;
+            string result = string.Empty;
+
+            List<int> listaValorOctal = new List<int>();
+
+            while (dividendo > 8)
+            {
+                resto = dividendo % divisor;
+                quociente = (int)dividendo / divisor;
+                dividendo = quociente;
+                listaValorOctal.Add(resto);
+            }
+
+            if (dividendo == 8)
+            {
+                listaValorOctal.Add(0);
+                listaValorOctal.Add(1);
+            }
+            else
+            {
+                listaValorOctal.Add(dividendo);
+            }
+
+            listaValorOctal.Reverse();
+            foreach (int valor in listaValorOctal)
+            {
+                result = result + "" + valor;
+            }
+
+            return result;
+
+        }
+
+        public int OctalDecimal(int nr)
+        {
+
+            int expoente = 0;
+            int numero;
+            int soma = 0;
+
+
+            string numeroInvertido = InverterString(textNr.Text);
+
+            for (int i = 0; i < numeroInvertido.Length; i++)
+            {
+
+                numero = Convert.ToInt32(numeroInvertido.Substring(i, 1));
+                soma += numero * (int)Math.Pow(8, expoente);
+                expoente++;
+
+            }
+
+            return soma;
+
+        }
+
+        public string DecimalHexa(int nr)
+        {
+
+            int dividendo = nr;
+            int divisor = 16;
+            string result = string.Empty;
+
+            List<int> listaValorConvertido = new List<int>();
+
+            while (dividendo >= divisor)
+            {
+                int resto = dividendo % divisor;
+                dividendo = (int)dividendo / divisor;
+
+                listaValorConvertido.Add(Convert.ToInt32(resto));
+
+            }
+
+            listaValorConvertido.Add((int)Math.Floor((double)dividendo));
+
+
+            List<string> listaHexa = new List<string>();
+
+            foreach (int valor in listaValorConvertido)
+            {
+                if (valor == 10)
+                {
+                    listaHexa.Add("A");
+                }
+                else if (valor == 11)
+                {
+                    listaHexa.Add("B");
+                }
+                else if (valor == 12)
+                {
+                    listaHexa.Add("C");
+                }
+                else if (valor == 13)
+                {
+                    listaHexa.Add("D");
+                }
+                else if (valor == 14)
+                {
+                    listaHexa.Add("E");
+                }
+                else if (valor == 15)
+                {
+                    listaHexa.Add("F");
+                }
+                else
+                {
+                    listaHexa.Add(valor + string.Empty);
+                }
+
+            }
+
+            listaHexa.Reverse();
+            foreach (string valor in listaHexa)
+            {
+                result = result + "" + valor;
+            }
+            return result;
+        }
+
+        public string HexaDecimal(string nr)
+        {            
+            string result = string.Empty;
+            nr = nr.ToUpper();
+            nr = InverterString(nr);
+            double valorDecimal = 0;
+
+            for (int i = 0; i < nr.Length; i++)
+            {
+                if (nr.Substring(i, 1).Equals("A"))
+                {
+                    valorDecimal = valorDecimal + Convert.ToInt32(10 * Math.Pow(16, i));
+                }
+                else if (nr.Substring(i, 1).Equals("B"))
+                {
+                    valorDecimal = valorDecimal + Convert.ToInt32(11 * Math.Pow(16, i));
+                }
+                else if (nr.Substring(i, 1).Equals("C"))
+                {
+                    valorDecimal = valorDecimal + Convert.ToInt32(12 * Math.Pow(16, i));
+                }
+                else if (nr.Substring(i, 1).Equals("D"))
+                {
+                    valorDecimal = valorDecimal + Convert.ToInt32(13 * Math.Pow(16, i));
+                }
+                else if (nr.Substring(i, 1).Equals("E"))
+                {
+                    valorDecimal = valorDecimal + Convert.ToInt32(14 * Math.Pow(16, i));
+                }
+                else if (nr.Substring(i, 1).Equals("F"))
+                {
+                    valorDecimal = valorDecimal + Convert.ToInt32(15 * Math.Pow(16, i));
+                }
+                else
+                {
+                    double a = Convert.ToInt32(nr.Substring(i, 1));
+                    valorDecimal = valorDecimal + Convert.ToInt32(a * Math.Pow(16, i));
+                }
+            }
+
+            return valorDecimal + string.Empty;
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void textNr_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boxBinario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void boxDecimal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boxHexa_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boxOctal_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
